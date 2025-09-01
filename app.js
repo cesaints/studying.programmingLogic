e7a2427831e45cbe49aaf865ecd6cf5c23dc16e0
Carlos Eduardo Gonçalves <carlossaintsdeveloper@gmail.com>
2025-09-01 12:54:35 -0300

    Starting studies with the information that we must store information through variables. Here messages were created when entering the game, to choose a number and check whether the number is smaller, larger or the correct number

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 alert('Boas vindas ao jogo do número secreto!');
+
+let secretNumber = 29;
+let chute = prompt('Escolha um número entre 0 e 30:');
+
+if (secretNumber == chute) {
+    alert('Parabéns! Você acertou!');
+} else if (chute > secretNumber) {
+    alert('O número secreto é menor que ' + chute);
+} else {
+    alert('O número secreto é maior que ' + chute);
+}
+
